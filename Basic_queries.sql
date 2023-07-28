e8a3cb14c34cfe4862d6f2d4d9bbc7b0a5354b02
kolaynurdan <kolaynurdan@gmail.com>
2023-07-28 15:06:54 +0300

    added new queries

--- Basic_queries.sql
+++ Basic_queries.sql
@@ -2,3 +2,10 @@
 
 select * from cd.facilities;
 
+--- You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
+
+select name, membercost from cd.facilities;
+
+--- How can you produce a list of facilities that charge a fee to members?
+
+select * from cd.facilities where membercost > 0;
