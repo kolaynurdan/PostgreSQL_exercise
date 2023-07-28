1a9cd37c93ac45bac00a86ba4071ad2fb1df1727
kolaynurdan <kolaynurdan@gmail.com>
2023-07-28 15:32:15 +0300

    added queries

--- Basic_queries.sql
+++ Basic_queries.sql
@@ -9,3 +9,14 @@ select name, membercost from cd.facilities;
 --- How can you produce a list of facilities that charge a fee to members?
 
 select * from cd.facilities where membercost > 0;
+
+--- How can you produce a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost? Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.
+
+select faid, name, membercost, monthlymaintenance from cd.facilities
+where membercost > 0 and (membercost < monthlymaintenance/50.0);
+
+--- How can you produce a list of all facilities with the word 'Tennis' in their name?
+
+select * from cd.facilities
+where name like '%Tennis%';
+
