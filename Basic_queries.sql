52ce793c8d2d8297b5e9c6b87fd46cfb07a6a495
kolaynurdan <kolaynurdan@gmail.com>
2023-07-28 15:45:28 +0300

    add queries

--- Basic_queries.sql
+++ Basic_queries.sql
@@ -20,3 +20,18 @@ where membercost > 0 and (membercost < monthlymaintenance/50.0);
 select * from cd.facilities
 where name like '%Tennis%';
 
+--- How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the OR operator.
+
+select * from cd.facilities
+where facid in (1,5);
+
+---  How can you produce a list of facilities, with each labelled as 'cheap' or 'expensive' depending on if their monthly maintenance cost is more than $100? Return the name and monthly maintenance of the facilities in question.
+
+select name, 
+	case when (monthlymaintenance > 100) then
+	   'expensive'
+	else
+	   'cheap'
+	end as cost
+	from cd.facilities
+	
